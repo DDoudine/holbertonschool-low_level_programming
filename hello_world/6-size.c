53f9cde0f533d88098ec319225ff106645e18b0c
DDoudine <amandinekemp@gmail.com>
2023-10-15 13:24:37 -0700

    supp 1 space in 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -19,5 +19,5 @@ printf("Size of a long int: %ld byte(s)\n", sizeof(long_int_type));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(long_long_int_type));
 printf("Size of a float: %ld byte(s)\n", sizeof(float_type));
 
- return (0);
+return (0);
 }
