48c8099f83971c7902721f635ee20ab7a8abd43b
DDoudine <amandinekemp@gmail.com>
2023-10-15 13:22:51 -0700

    add () in return in 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,7 +4,7 @@
  * main - Entry
  * show sizes
  * Return: 0
- */ 
+ */
 int main(void)
 {
 char char_type;
@@ -12,12 +12,12 @@ int int_type;
 long int long_int_type;
 long long int long_long_int_type;
 float float_type;
- 
+
 printf("Size of a char: %ld byte(s)\n", sizeof(char_type));
 printf("Size of an int: %ld byte(s)\n", sizeof(int_type));
 printf("Size of a long int: %ld byte(s)\n", sizeof(long_int_type));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(long_long_int_type));
 printf("Size of a float: %ld byte(s)\n", sizeof(float_type));
 
- return 0;
+ return (0);
 }
