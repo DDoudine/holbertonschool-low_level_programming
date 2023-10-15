a4eacd8488d860315eca5312b73d140478eaba6d
DDoudine <amandinekemp@gmail.com>
2023-10-15 01:10:24 -0700

    add description for function in 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/*
+fonction main: La fonction main sert de point de départ pour l'exécution du programme
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts(""Programming is like building a multilingual puzzle\n");
 return (0);
 }
