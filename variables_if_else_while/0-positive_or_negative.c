8194af256d81286c304661ee1149d0e4b4d7efb7
DDoudine <amandinekemp@gmail.com>
2023-10-16 12:22:15 +0200

    supp space and indent in 0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,12 @@
 #include <time.h>
 /* more headers goes there */
 
+/**
+ * main-Entry
+ *
+ * Return: 0
+ */
+
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -14,17 +20,17 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-  printf("%d is positive\n", n);
+printf("%d is positive\n", n);
 }
 
- else if (n == 0)
+else if (n == 0)
 {
-  printf("%d is zero\n", n);
+printf("%d is zero\n", n);
 }
 
 else
 {
-  printf("%d is negative\n", n);
+printf("%d is negative\n", n);
 }
 
 return (0);
