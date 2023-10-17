30cc2cbd0846b5d1e3d9b01c08024311d7a39089
DDoudine <amandinekemp@gmail.com>
2023-10-17 20:39:11 +0200

    supp return and space in 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,15 +3,17 @@
 
 /**
  * print_alphabet_minuscule - Entry
- *
+ * Function print_alphabet
  * Return: 0
  */
 void print_alphabet(void)
 {
 	int letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
+
 	{
 		putchar(letter);
 	}
-	return;
+	putchar('\n');
 }
