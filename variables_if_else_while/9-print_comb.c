a2adeaf43fc05cc4b25f0f3fe79bb877c2d674b5
DDoudine <amandinekemp@gmail.com>
2023-10-17 11:12:14 +0200

    add putchar , in 9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int num = 0;
+	for (num = 0; num <= 9; num++)
+		{
+		putchar('0' + num);
+		putchar(',');
+		}
+	putchar('\n');
+	return (0);
+}
