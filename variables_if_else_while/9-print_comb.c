3aa48e4f60f9225bb7bd873d5c05a588b5028fba
DDoudine <amandinekemp@gmail.com>
2023-10-17 12:51:06 +0200

    add if 9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,13 +8,22 @@
  */
 int main(void)
 {
-int num = 0;
-	for (num = 0; num < 10; num++)
-		{
-		putchar('0' + num);
-		putchar(',');
-		putchar(' ');
-		}
+	int num;
+	
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+
+		if (num != '9')
+                {
+
+                        putchar(',');
+                        putchar(' ');
+
+                }	
+
+	}	
+	putchar('\n');
 	return (0);
 }
 
