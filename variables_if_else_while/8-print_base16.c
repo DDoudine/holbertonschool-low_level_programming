329e457c4eca9669933d407b1c067f9763738cd1
DDoudine <amandinekemp@gmail.com>
2023-10-17 10:47:15 +0200

    add + 8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,11 +9,15 @@
 int main(void)
 {
 int num = 0;
-while (num < 16)
-	{
-	putchar('0' + num);
-	num++;
-	}
-	putchar('\n');
-return (0);
+char letter;
+	for (num = 0; num <= 9; num++)
+		{
+		putchar('0' + num);
+		}
+	for (letter = 'a'; letter <= 'f'; letter++)
+		{
+		putchar(letter);
+		putchar('\n');
+		}
+	return (0);
 }
