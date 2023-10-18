2861602f2dae0c3262cafa4c6a630291c2da8040
DDoudine <amandinekemp@gmail.com>
2023-10-18 13:19:17 +0200

    add 3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/* more headers goes there */
+
+/**
+ * _islower - Entry
+ * @c: character
+ *
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
