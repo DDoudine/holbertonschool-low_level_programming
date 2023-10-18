06ec8b9c707e0aca3a6c5ac04488ee009cf533db
DDoudine <amandinekemp@gmail.com>
2023-10-18 16:26:04 +0200

    betty 6-abs.s

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 
+/*la valeur absolue de –4 est 4, et celle de +4 est 4*/
+
 /**
- * la valeur absolue de –4 est 4, et celle de +4 est 4
- *
  * _abs - Entry
  * @n: number
- * 
  * Return: Number
  */
 int _abs(int n)
