dcbef60af604ee38e3606935ca324341bcfe7b89
DDoudine <amandinekemp@gmail.com>
2023-10-18 10:45:33 +0200

    add index ++ in 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,14 @@
 void print_alphabet_x10(void)
 {
 	int index = 0;
+	while (index < 10)
 	{
-		while (index < 10)
 		char letter;
 		for (letter = 'a'; letter <= 'z'; ++letter)
 		{
 			putchar(letter);
 		}
 	putchar('\n');
+	index++;
 	}
 }
