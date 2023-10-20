0accf302db3d564c88beb5f3ca2823ef74749081
DDoudine <amandinekemp@gmail.com>
2023-10-20 17:27:03 +0200

    add 3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+char num = 0;
+while (num < 10)
+{
+putchar('0' + num);
+num++;
+}
+putchar('\n');
+return (0);
+}
