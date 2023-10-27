9fa38e31295bc11bb05992bd2682e05d6058cf23
DDoudine <amandinekemp@gmail.com>
2023-10-27 12:36:19 +0200

    add 4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reverse_array - entry
+ * @a: pointer
+ * @n: integer
+ */
+void reverse_array(int *a, int n)
+{
+	int index; 
+	int entier;
+
+	n = n - 1;
+
+	for (index = 0; index < n; index++, n--)
+	{
+		entier = a[index];
+		a[index] = a[n];
+		a[n] = entier;
+	}
+}
+
