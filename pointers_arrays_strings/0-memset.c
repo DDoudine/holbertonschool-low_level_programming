ba5f4f4f00ae113bedaeb1b9eea1b20b9f2ae427
DDoudine <amandinekemp@gmail.com>
2023-10-29 15:50:28 +0100

    add comment

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -3,17 +3,19 @@
 
 /**
  * _memset - ecrivez une fonction qui remplit la memoire avec un octet constant
- * @s: pointer
- * @b: character
- * @n: unsigned integer
+ * @s: memoire a remplir
+ * @b: character a mettre dans la memoire
+ * @n: nombre de fois a mettre
  * Return: s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	char c = b;
 	char *ch;
 
-	for (ch = s; n > 0; ++ch, --n)
-		*ch = c;
+	for (ch = s; n > 0; --n)
+	{
+		*ch = b;
+		++ch;
+	}
 	return (s);
 }
