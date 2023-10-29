374678f175a4a6814c1dbe54faf8ae2b55171a68
DDoudine <amandinekemp@gmail.com>
2023-10-29 11:37:56 +0100

    add 1-memcpy.c

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memcpy - une fonction qui copie la zone mémoire
+ * @dest: pointer
+ * @src: pointer
+ * @n: unsigned integer
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *d = dest;
+	char *s = src;
+	int i;
+
+	for (i = n - 1; i >= 0; i--)
+		d[i] = s[i];
+	return (dest);
+}
