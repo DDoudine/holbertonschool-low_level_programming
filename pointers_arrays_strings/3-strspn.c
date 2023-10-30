9a01e20e79a0ea1f341ea6240bfd0fb3c8f3ab4f
DDoudine <amandinekemp@gmail.com>
2023-10-30 15:15:05 +0100

    add comment in 3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -2,29 +2,30 @@
 #include "main.h"
 
 /**
-  5  * _strspn - une fonction qui obtient la longueur d'une sous-chaîne de préfixe.
-  6  * @s: chaine de caractere entiere
-  7  * @accept: liste des caracteres qu on accepte de compter
-  8  * Return: s
-  9  */
+  * _strspn - une fonction qui obtient la long d'une ss-chaîne de préfixe
+  * @s: chaine de caractere entiere
+  * @accept: liste des caracteres qu on accepte de compter
+  * @PointeurVChaine: Pointeur vers chaine
+  * Return: s
+  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	char *pointeur_vers_chaine, *pointeur_vers_debut_chaine = s;
-	char caractere_courant;
+	char *PointeurVChaine, *PointeurVersDebutChaine = s;
+	char CaractereCourant;
 
 	/* parcourir les caracteres de la chaine */
 	/* pour chaque caractere */
-	while ((caractere_courant = *s++) != '\0')
+	while ((CaractereCourant = *s++) != '\0')
 	{
 		 /*je cherche si c'est dans accept*/
-		for (pointeur_vers_chaine = accept; *pointeur_vers_chaine != '\0'; ++pointeur_vers_chaine)
-			if (*pointeur_vers_chaine == caractere_courant)
+		for (PointeurVChaine = accept; *PointeurVChaine != '\0'; ++PointeurVChaine)
+			if (*PointeurVChaine == CaractereCourant)
 			break;
 		/*verifie la correspondance du caractere de la chaine*/
-		if (*pointeur_vers_chaine == '\0')
-			return (s - pointeur_vers_debut_chaine - 1);
+		if (*PointeurVChaine == '\0')
+			return (s - PointeurVersDebutChaine - 1);
 	}
-	return (s - pointeur_vers_debut_chaine - 1);
+	return (s - PointeurVersDebutChaine - 1);
 }
 
