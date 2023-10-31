96e580ba13b6ee41837ea96ade93df089be241b7
DDoudine <amandinekemp@gmail.com>
2023-10-31 17:14:00 +0100

    add 4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - calcule la puissance d un nombre de maniere recursive
+ * @x: base de la puissance
+ * @y: exposant
+ * Return: le resultat de x eleve a la puissance y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+    		/*recursion pour les puissances positives*/
+		return x * _pow_recursion(x, y - 1);
+	else if (y < 0)
+		/*valeur indefinie pour les exposants neg*/
+		return -1;
+	else
+		/*tout nombre eleve a la puissance 0 est egal a 1*/
+		return 1;
+}
