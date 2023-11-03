3b635b698ad26bc2d81b6556dcea79488e11d08e
DDoudine <amandinekemp@gmail.com>
2023-11-03 22:46:19 +0100

    add ifndef in main.h

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 
 int _putchar(char c);
@@ -20,4 +23,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
 
