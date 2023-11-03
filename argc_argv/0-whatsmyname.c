666cd3e4e97602e2183d3cb427ecc4768ec73ddd
DDoudine <amandinekemp@gmail.com>
2023-11-03 17:26:53 +0100

    betty for 0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,15 +1,26 @@
-#include <stdio.h>  // inclure la bibliotheque standard pour l entree/sortie
+#include <stdio.h>  /*inclure la bibliotheque standard pour l entree/sortie*/
 
-// declaration de la fonction principale
-int main(int argc, char *argv[]) {
-	int index;  // declaration d une variable pour la boucle
+/**
+ * main - prints the program name and command-line arguments
+ * @argc: integer - Number of command-line arguments
+ * @argv: pointer to an array of strings - array of command-line arguments
+ *
+ * return: 0 (success)
+ */
 
-	// boucle pour parcourir les arguments de la ligne de commande
-	for (index = 0; index < argc; index++) {
-		printf("%s", argv[index]);  // afficher chaque argument
+
+/* declaration de la fonction principale */
+int main(int argc, char *argv[])
+{
+	int index;  /* declaration d une variable pour la boucle */
+
+	/* boucle pour parcourir les arguments de la ligne de commande */
+	for (index = 0; index < argc; index++)
+{
+		printf("%s", argv[index]);  /*afficher chaque argument*/
 	}
 
-	printf("\n");  // saut de ligne
-	return 0;  // indiquer une execution reussie
+	printf("\n");  /*saut de ligne*/
+	return (0);  /*indiquer une execution reussie*/
 }
 
