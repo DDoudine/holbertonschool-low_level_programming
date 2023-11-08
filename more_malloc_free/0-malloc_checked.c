bd226358c9492cc17267b4f2abc2451b15df943a
DDoudine <amandinekemp@gmail.com>
2023-11-08 17:02:13 +0100

    add comment 0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,18 +3,21 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - une fonction qui alloue de la mémoire à l'aide de malloc.
- * @b: unsigned integer
+ * malloc_checked - une fonction qui alloue de la mémoire à l'aide de malloc
+ * @b: taille de la memoire a allouer
  *
- * Return: pointer
+ * Return: pointeur ver la memoire allouee
  */
 
 void *malloc_checked(unsigned int b)
 {
+	 /*alloue de la memoire de la taille specifiee*/
 	int *ptr = malloc(b);
 
+	/*Verifie si l allocation a echoue (pointeur NULL)*/
 	if (ptr == NULL)
-		exit(98);
-	return (ptr);
+		exit(98);/*en cas d echec, code d erreur 98*/
+
+	return (ptr);/*retourne un pointeur vers la memoire allouee en cas de succes*/
 }
 
