8ccf862a3f23631213a4d04bdf84e0703de26dd0
DDoudine <amandinekemp@gmail.com>
2023-11-09 20:01:34 +0100

    nmemb 2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,29 +1,36 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _calloc - allocate memory for an array
- * @nmemb:   number of elements in the array
- * @size:    size in bytes of the elements
+ * _calloc - Alloue et initialise à zéro un tableau en mémoire dynamique.
+ * @nmemb: Nombre d'éléments dans le tableau.
+ * @size: Taille en octets de chaque élément du tableau.
  *
- * Return:   pointer to allocate memory
+ * Return: Pointeur vers la mémoire allouée, NULL en cas d'échec.
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	/* Pointeur mémoire */
 	char *ptr;
-	unsigned int count;
+	unsigned int i;
 
+	/* Vérifie les conditions d'allocation */
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(nmemb * size);
+	/* Alloue la mémoire pour le tableau */
+	ptr = malloc(size * nmemb);
 
+	/* Vérifie l'allocation de mémoire */
 	if (ptr == NULL)
 		return (NULL);
 
-	for (count = 0; count < (nmemb * size); count++)
-		ptr[count] = 0;
+	/* Initialise les éléments à zéro */
+	for (i = 0; i < (size * nmemb); i++)
+		ptr[i] = '\0';
 
+	/* Retourne le pointeur vers la mémoire allouée */
 	return (ptr);
 }
 
