a6d47781fffb4a0b578b3ff2411ef1b52df9fa00
DDoudine <amandinekemp@gmail.com>
2023-11-09 20:22:42 +0100

    add

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -9,30 +9,23 @@
 
 int *array_range(int min, int max)
 {
-	/* Pointeur pour stocker l'adresse du tableau d'entiers */
 	int *array;
 	int i, len;
 
-	/* Pointeur pour stocker l'adresse du tableau d'entiers */
 	if (min > max)
 		return (NULL);
 
-	/* Calcule la longueur du tableau */
 	for (len = 0; len < (max - min); len++)
 		;
 
-	/* Alloue de la mémoire pour le tableau d'entiers */
 	array = malloc(sizeof(int) * (len + 1));
 
-	/* Vérifie si l'allocation de mémoire a réussi */
 	if (array == NULL)
 		return (NULL);
 
-	/* Initialise le tableau avec les valeurs de la plage spécifiée */
 	for (i = 0; i <= len; i++)
 		array[i] = min++;
 
-	/* Retourne le tableau d'entiers */
 	return (array);
 }
 
