75a890b49779fc364800469511b979a67de1c36d
DDoudine <amandinekemp@gmail.com>
2023-11-09 19:21:58 +0100

    nmemb 2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 /**
- * _calloc - Alloue de la memoire pour un tableau
- * @num_elements: Nombre d éléments dans le tableau
- * @element_size: Taille en octets des éléments
+ * _calloc - alloue de la memoire pour un tableau
+ * @nmemb: nombre d'éléments dans le tableau
+ * @size: taille en octets des éléments
  *
  * Return:   pointeur vers la mémoire allouée
  */
@@ -16,21 +16,21 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int index;
 
 	/* Vérifier si num_elements ou element_size sont nuls */
-	if (num_elements == 0 || element_size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	/* Allouer de la mémoire pour le tableau */
-	allocated_memory = malloc(num_elements * element_size);
+	ptr = malloc(nmemb * size);
 
 	/* Vérifier si l'allocation de mémoire a réussi */
-	if (allocated_memory == NULL)
-	return (NULL);
+	if (ptr == NULL)
+		return (NULL);
 
 	/* Initialiser tous les éléments du tableau à zéro */
-	for (index = 0; index < (num_elements * element_size); index++)
+	for (index = 0; index < (nmemb * size); index++)
 	{
-	allocated_memory[index] = 0;
+		ptr[index] = 0;
 	}
-	return (allocated_memory);
+	return (ptr);
 }
 
