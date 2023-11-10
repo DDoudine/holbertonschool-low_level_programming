02557e4b8444eb00f4972c4e020bae7866f52537
DDoudine <amandinekemp@gmail.com>
2023-11-10 11:58:03 +0100

    add 3-function_like_macro.h

---
+++ preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
+#define ABS(x) ((x >= 0) ? (x) : -(x))
+
+#endif
+
+/**
+ * Dans cette macro, ABS(x) renvoie la valeur absolue de x.
+ * Elle utilise une expression conditionnelle pour vérifier 
+ * si x est supérieur ou égal à zéro. 
+ * Si c'est le cas, il renvoie simplement x, sinon, il renvoie -x
+ */
+
