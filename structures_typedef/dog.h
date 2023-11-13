6b6ddee2145ad550f21c057e0bd628f17141b9a7
DDoudine <amandinekemp@gmail.com>
2023-11-13 16:12:52 +0100

    add dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -18,9 +18,14 @@ struct dog
 	char *owner;
 };
 
+/* Alias pour faciliter l'utilisation de la structure*/
+typedef struct dog;
+
 /*Prototypes des fonctions pour travailler avec la structure dog*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+#endif
 
