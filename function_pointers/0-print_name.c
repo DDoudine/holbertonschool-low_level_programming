5a1b31bc17bb4db09ef564343b83ac27279fa1d6
DDoudine <amandinekemp@gmail.com>
2023-11-14 10:28:08 +0100

    forget s in #include " "

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,24 +3,21 @@
 #include <stdlib.h>
 
 /**
- * print_name - imprime le nom
+ * print_name - fonction qui imprime le nom
  * @name: pointeur vers le nom à imprimer
  * @f: pointeur vers une focntion prenant un pointeur de caractere en tant que parametre
  * 
  * Description: 
- * Cette fonction prend un nom (une chaine de caratere) et un pointeur de fonction comme arguments
- * Elle verifie d'abord si le pointeur de fonction est NULL
- * Si c'est le cas, elle termine le programme avec le code de sortie EXT_SUCESS
- * Sinon, elle appelle la fonction pointee par le pointeur de fonction avec le nom comme argument
-*/
+ * cette fonction prend un pointeur vers une chaine de caracteres (`name`) 
+ * et un pointeur vers une fonction (`f`) en tant que parametres
+ * Si le pointeur de fonction n est pas NULL, 
+ * la fonction pointee par le pointeur de fonction est appelee 
+ * avec le nom comme argument
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
-    /*verifie si le pointeur de fonction est NULL*/
-    if (f == NULL)
-        exit(EXIT_SUCESS);
-
-
-    /* apelle la fonction pointee par le pointeur de fonction avec le nom comme argument*/
-    f(name);
+    /*verifie si le pointeur de fonction n est pas NULL*/
+    if (f != NULL)
+        f(name);
 }
