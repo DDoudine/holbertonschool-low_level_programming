742a0d88edc0d8faf071c1ec57b9724d434a4e2e
DDoudine <amandinekemp@gmail.com>
2023-12-06 10:10:19 +0100

    add fonction strlen

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,41 +1,64 @@
 #include "main.h"
 
 /**
+ * _strlen - calcule la longueur d une chaine de caracteres
+ * @s: chaine de caracteres
+ *
+ * Return: longueur de la chaine
+ */
+
+int _strlen(char *s)
+{
+	int lenght = 0;
+
+	while (*s != '\0')
+	{
+		lenght++;
+		s++;
+	}
+	return (lenght);
+}
+
+/**
  * create_file - cree un fichier
  * @filename: nom du fichier
  * @text_content: contenu du fichier
  *
- * Return: -1 en cas d erreur, 1 en cas de succes
+ * Return: 1 en cas de succes, -1 en cas d echec
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor, write_status;
+	int file_descriptor, length = 0, write_status;
 
-	/*verifie si le nom du fichier est nul*/
+	/*Vérifie si le nom du fichier est nul*/
 	if (filename == NULL)
 		return (-1);
 
-	/*Ouvre ou cree le fichier en mode ecriture uniquement,*/
-	/*le tronque à zéro et definit les permissions à 0600*/
+	/*Ouvre le fichier en mode écriture uniquement, le crée ou le tronque,*/
+	/*et définit les permissions à 0600*/
 	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file_descriptor == -1)
 		return (-1);
 
-	/*verifie si le contenu du texte est nul*/
+	/*Vérifie si le contenu du texte n'est pas nul*/
 	if (text_content != NULL)
 	{
-		write_status = write(file_descriptor, text_content, strlen(text_content));
-		return (1);
+		/*Obtient la longueur du texte*/
+		length = _strlen(text_content);
 
-		/*verifie si l'ecriture a échoué*/
+		/*Écrit le contenu du texte dans le fichier*/
+		write_status = write(file_descriptor, text_content, length);
+
+		/*Vérifie si l'écriture a échoué*/
 		if (write_status == -1)
 		{
 			close(file_descriptor);
 			return (-1);
 		}
 	}
-	/*ferme le fichier*/
+
+	/*Ferme le fichier*/
 	close(file_descriptor);
 
 	return (1);
