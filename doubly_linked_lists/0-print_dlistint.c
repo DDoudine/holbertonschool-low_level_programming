5803f59912921925d3cca7bee65bb974bb9d8ee3
DDoudine <amandinekemp@gmail.com>
2023-12-17 13:34:50 +0100

    change variable h in head

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,21 +2,21 @@
 
 /**
  * print_dlistint - Imprime tous les éléments d'une liste dlistint_t
- * @h: Pointeur vers la liste
+ * @head: Pointeur vers la liste h=header
  *
  * Return: le nombre de noeuds dans la liste
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t print_dlistint(const dlistint_t *head)
 {
 	/*variable pour compter le nombre de noeuds*/
 	int element_count = 0;
 
-	while (h != NULL)
+	while (head != NULL)
 	{
 		/*impression de la valeur du noeud*/
-		printf("%d\n", h->n);
+		printf("%d\n", head->n);
 		/*déplacement vers le noeud suivant*/
-		h = h->next;
+		head = head->next;
 		/*incrémentation du compteur de noeuds*/
 		element_count++;
 	}
